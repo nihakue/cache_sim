76b4b5729cd38b4b77360ae86c0b77c99dba8151
nihakue <walden.gabrielw@gmail.com>
2014-03-05 19:30:05 +0000

    decode class method for class

--- cache_sim/cache_sim.py
+++ cache_sim/cache_sim.py
@@ -1,7 +1,10 @@
 from trace_read import trace_read
 
-BLOCK_SIZE = 0xFF
+BLOCK_SIZE = 32 #Bytes
 MAX_ADDR = 0xFFFFFFFFFFFF
+ADDRESS_SIZE = 48
+
+
 
 class Cache(object):
     """The class implements a cache. Use the cache's simulate function
@@ -9,13 +12,23 @@ class Cache(object):
 
     :param: assoc = associativity of the cache.
     :type: int
-    :param: sets = number of sets in the cache. This in part determines the size of the cache.
+    :param: sets = number of sets in the cache.
+    :type: int
+    :param: block_size size of one cache blocks
     :type: int
     """
-    def __init__(self, assoc, sets):
+    def __init__(self, assoc=1, sets=128, block_size=32, blocks=128):
+        assert all((x % 2 == 0
+                   for x in [sets, block_size, blocks]))
+        assert assoc is 1 or assoc % 2 == 0
+
         self._assoc = assoc
         self._sets = sets
-        self._kb = self._calculate_size()
+        self._block_size = block_size
+        self._size = self._calculate_size()
+        self._index_bits = len(bin(sets)[2:])
+        self._offset_bits = 5
+
         self.w_hits = 0
         self.r_hits = 0
         self.n_ops = 0
@@ -27,11 +40,20 @@ class Cache(object):
         pass
 
     def _calculate_size(self):
-        return 4 * self._sets
+        '''Calculates the cache size in bytes'''
+        return self._sets * self._assoc * self._block_size
 
-    def _decode(self, raw_addr):
+    def decode(self, raw_addr):
         '''returns the tag, index, and offset for a raw address'''
+        addr_bits = bin(int(raw_addr, 16))[2:]
+        addr_bits = addr_bits.zfill(ADDRESS_SIZE)
+        tag_end = ADDRESS_SIZE - self._offset_bits - self._index_bits
 
+        tag = addr_bits[:tag_end]
+        index = addr_bits[tag_end:tag_end+self._index_bits]
+        offset = addr_bits[-self._offset_bits:]
+        assert len(tag + index + offset) == len(addr_bits)
+        return (tag, index, offset)
 
     def simulate(tracefile):
         '''Simulates this cache on a memory trace file. tracefile should
